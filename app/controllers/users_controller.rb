3a7d710c1d8e9ba599f26a817c36796f83a6ac97
i-muiri <ianmuiri99@gmail.com>
2023-03-28 23:41:20 +0300

    done test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(params[:id])
+        render json: user, include: :items, except: [:created_at, :updated_at]
+    end
 end
